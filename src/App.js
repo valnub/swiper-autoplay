bc4538c51b66a74e0d1aae03f539fcfe5492c7d0
Timo <mail@timo-ernst.net>
2022-02-06 00:16:00 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+/* eslint-disable import/no-anonymous-default-export */
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import './App.css';
 
-function App() {
+import 'swiper/css/autoplay';
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function imgUrl() {
+  const id = rand(1, 200);
+  return `https://picsum.photos/id/${id}/1920/1080`;
+}
+
+function createSlide() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SwiperSlide>
+      <img className="img" src={imgUrl()} alt="" />
+    </SwiperSlide>
   );
 }
 
-export default App;
+export default () => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      slidesPerView={1}
+      navigation
+      autoplay={{ delay: 1000 }}
+      pagination={{ clickable: true }}
+    >
+      {createSlide()}
+      {createSlide()}
+      {createSlide()}
+      {createSlide()}
+    </Swiper>
+  );
+};
